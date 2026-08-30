247a6fb4a0c97778e7b0fbdc7dcfb00f3bd19833
BezaleelPaul <145212633+BezaleelPaul@users.noreply.github.com>
2026-08-30 18:08:27 +0530

    Redeploy with correct base-href /veltrix-sports/

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1254754698" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "585187823" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
